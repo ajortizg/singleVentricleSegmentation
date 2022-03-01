224c9cae34aa13efdd54ea02049501060e22f1d7
toniortiz <toniortizg@hotmail.com>
2022-03-01 18:05:35 +0100

    Minimization procedure of u and v

--- main.py
+++ main.py
@@ -41,3 +41,8 @@ alg.compute(source, target)
 # # sum = uv + grid
 # # print("sum\n", sum)
 # # print(sum.shape)
+
+# x = np.array([[1, 2, 3],
+#               [4, 5, 6],
+#               [7, 8, 9]])
+# print(x**2)
