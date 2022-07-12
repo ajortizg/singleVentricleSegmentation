b1e7647a97b45870f5e117a504ba5769daa58392
Stefan Simon <stefansimon7@gmail.com>
2022-07-12 17:07:01 +0200

    minor

--- TVL1OF/TVL1OF3D.py
+++ TVL1OF/TVL1OF3D.py
@@ -11,18 +11,21 @@ import torch
 import time
 from tqdm import tqdm
 
-
-sys.path.append("../utils")
-from utils.plots import *
+utils_lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../utils'))
+sys.path.append(utils_lib_path)
+#sys.path.append("../utils")
+#from utils.plots import *
+import torch_utils as tu
 # from utils.config import *
-from utils.flow_viz import *
+#from utils.flow_viz import *
+import flow_viz
 
 # sys.path.append("../pythonOps/")
 # from pythonOps.mesh import *
 # from pythonOps.differentialOps import *
 pythonOps_lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../pythonOps'))
 sys.path.append(pythonOps_lib_path)
-import utils.torch_utils as tu
+#import utils.torch_utils as tu
 # import mesh
 # import differentialOps
 
