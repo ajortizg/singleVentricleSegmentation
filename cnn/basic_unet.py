bug in gamma scaling solved

cnn/basic_unet.py
@@ -4,25 +4,35 @@ import torch.nn.functional as F
 
 
 class BasicUNet3d(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+    def __init__(self, config, logger):
         super(BasicUNet3d, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
 
-        self.inc = DoubleConv(n_channels, 16)
-        self.down1 = Down(16, 32)
-        self.down2 = Down(32, 64)
-        self.down3 = Down(64, 128)
+        n_classes = config.getint('PARAMETERS', 'NUM_CLASSES')
+        n_channels = config.getint('PARAMETERS', 'INPUT_CHANNELS')
+        features_start = config.getint('PARAMETERS', 'FEATURES_START')
+        bilinear = config.getboolean('PARAMETERS', 'TRILINEAR')
+        self.residual = config.getboolean('PARAMETERS', 'RESIDUAL')
+
+        feats = [features_start]
+        for i in range(4):
+            feats.append(feats[-1] * 2)
+
+        self.inc = DoubleConv(n_channels, feats[0])
+        self.down1 = Down(feats[0], feats[1])
+        self.down2 = Down(feats[1], feats[2])
+        self.down3 = Down(feats[2], feats[3])
         factor = 2 if bilinear else 1
-        self.down4 = Down(128, 256 // factor)
-        self.up1 = Up(256, 128 // factor, bilinear)
-        self.up2 = Up(128, 64 // factor, bilinear)
-        self.up3 = Up(64, 32 // factor, bilinear)
-        self.up4 = Up(32, 16, bilinear)
-        self.outc = OutConv(16, n_classes)
+        self.down4 = Down(feats[3], feats[4] // factor)
+        self.up1 = Up(feats[4], feats[3] // factor, bilinear)
+        self.up2 = Up(feats[3], feats[2] // factor, bilinear)
+        self.up3 = Up(feats[2], feats[1] // factor, bilinear)
+        self.up4 = Up(feats[1], feats[0], bilinear)
+        self.outc = OutConv(feats[0], n_classes)
 
     def forward(self, x):
+        if self.residual:
+            identity = x[:, 1:2, :, :, :].clone()
+
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
@@ -33,7 +43,11 @@ class BasicUNet3d(nn.Module):
         x = self.up3(x, x2)
         x = self.up4(x, x1)
         logits = self.outc(x)
-        return torch.sigmoid(logits), logits
+
+        if self.residual:
+            return logits + identity, logits
+        else:
+            return logits, logits
 
 
 class DoubleConv(nn.Module):
@@ -44,10 +58,10 @@ class DoubleConv(nn.Module):
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
-            nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1),
             nn.InstanceNorm3d(mid_channels),
             nn.ReLU(),
-            nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1),
             nn.InstanceNorm3d(out_channels),
             nn.ReLU()
         )

cnn/unet_3d.py
@@ -168,39 +168,7 @@ class Up3d(nn.Module):
         return self.conv(x)
 
 
-class BasicUnet3d(nn.Module):
-    def __init__(self, config, logger):
-        super().__init__()
-        num_classes = config.getint('PARAMETERS', 'NUM_CLASSES')
-        input_channels = config.getint('PARAMETERS', 'INPUT_CHANNELS')
-        features_start = config.getint('PARAMETERS', 'FEATURES_START')
-        act = config.get('PARAMETERS', 'ACTIVATION')
-        slope = config.getfloat('PARAMETERS', 'ACTIVATION_SLOPE')
-        self.residual = config.getboolean('PARAMETERS', 'RESIDUAL')
-
-        features = [features_start]
-        for _ in range(1, 5):
-            features.append(features[-1] * 2)
-        features.append(features_start)
-
-        self.unet = BasicUNet(spatial_dims=3, in_channels=input_channels, out_channels=num_classes,
-                              act=("LeakyReLU", {"negative_slope": slope, "inplace": False}),
-                              norm=("instance", {"affine": False}),
-                              features=features)
-
-    def forward(self, x):
-        if self.residual:
-            identity = x[:, 1:2, :, :, :].clone()
-
-        x = self.unet(x)
-
-        if self.residual:
-            return x + identity, x
-        else:
-            return x, x
-
-
-class ResUnet3d(nn.Module):
+class ResUNet3d(nn.Module):
     def __init__(self, config, logger):
         super().__init__()
         num_layers = config.getint('PARAMETERS', 'NUM_LAYERS')
@@ -232,3 +200,35 @@ class ResUnet3d(nn.Module):
             return x + identity, x
         else:
             return x, x
+
+
+# class BasicUnet3d(nn.Module):
+#     def __init__(self, config, logger):
+#         super().__init__()
+#         num_classes = config.getint('PARAMETERS', 'NUM_CLASSES')
+#         input_channels = config.getint('PARAMETERS', 'INPUT_CHANNELS')
+#         features_start = config.getint('PARAMETERS', 'FEATURES_START')
+#         act = config.get('PARAMETERS', 'ACTIVATION')
+#         slope = config.getfloat('PARAMETERS', 'ACTIVATION_SLOPE')
+#         self.residual = config.getboolean('PARAMETERS', 'RESIDUAL')
+
+#         features = [features_start]
+#         for _ in range(1, 5):
+#             features.append(features[-1] * 2)
+#         features.append(features_start)
+
+#         self.unet = BasicUNet(spatial_dims=3, in_channels=input_channels, out_channels=num_classes,
+#                               act=("LeakyReLU", {"negative_slope": slope, "inplace": False}),
+#                               norm=("instance", {"affine": False}),
+#                               features=features)
+
+#     def forward(self, x):
+#         if self.residual:
+#             identity = x[:, 1:2, :, :, :].clone()
+
+#         x = self.unet(x)
+
+#         if self.residual:
+#             return x + identity, x
+#         else:
+#             return x, x

utils/transforms.py
@@ -448,9 +448,6 @@ class RandomRotate:
 
     def generate_rotation_grid(self, rot, offset, NZ, NY, NX):
         zz, yy, xx = np.meshgrid(np.arange(NZ), np.arange(NY), np.arange(NX), indexing="ij")
-        # grid = np.stack((xx, yy, zz), axis=0)
-
-        # grid_t = np.zeros((3, NZ, NY, NX))
         xx_t = (rot[0, 0] * xx + rot[0, 1] * yy + rot[0, 2] * zz) + offset[0]
         yy_t = (rot[1, 0] * xx + rot[1, 1] * yy + rot[1, 2] * zz) + offset[1]
         zz_t = (rot[2, 0] * xx + rot[2, 1] * yy + rot[2, 2] * zz) + offset[2]
@@ -519,24 +516,6 @@ class Erode:
         return f"{self.__class__.__name__}()"
 
 
-# class Resize:
-#     def __init__(self, size: tuple[int, int, int]):
-#         self.size = size
-
-#     def __call__(self, vol: torch.Tensor, ms: torch.Tensor, md: torch.Tensor):
-#         NZ, NY, NX, NT = vol.shape
-#         nsize = (1, 1, NZ, NY, NX)
-#         ms_i = F.interpolate(ms.reshape(nsize), size=self.size, align_corners=True, mode='trilinear').squeeze()
-#         md_i = F.interpolate(md.reshape(nsize), size=self.size, align_corners=True, mode='trilinear').squeeze()
-#         vol_i = torch.zeros(size=(*self.size, NT), dtype=vol.dtype, device=vol.device)
-#         for t in range(NT):
-#             vol_i[:, :, :, t] = F.interpolate(vol[:, :, :, t].reshape(nsize), size=self.size, align_corners=True, mode='trilinear').squeeze()
-#         return (vol_i, ms_i, md_i)
-
-#     def __repr__(self) -> str:
-#         return f"{self.__class__.__name__}()"
-
-
 class Resize:
     def __init__(self, size: tuple[int, int, int], boundary='mirror'):
         self.size = size
@@ -560,22 +539,6 @@ class Resize:
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}()"
-
-
-# class ResizeFlow3d:
-#     def __init__(self, size: tuple[int, int, int]):
-#         self.size = size
-
-#     def __call__(self, uvw: torch.Tensor):
-#         NZ, NY, NX, C = uvw.shape
-#         nsize = (1, 1, NZ, NY, NX)
-#         uvw_i = torch.zeros(size=(*self.size, C), dtype=uvw.dtype, device=uvw.device)
-#         for c in range(C):
-#             uvw_i[:, :, :, c] = F.interpolate(uvw[:, :, :, c].reshape(nsize), size=self.size, align_corners=True, mode='trilinear').squeeze()
-#         return uvw_i
-
-#     def __repr__(self) -> str:
-#         return f"{self.__class__.__name__}()"
 
 
 def rotx(deg):
@@ -741,6 +704,39 @@ def rot2d(deg):
 #             return (np.swapaxes(img4d_rot, 0, 2), np.swapaxes(ms_rot, 0, 2), np.swapaxes(md_rot, 0, 2))
 #         else:
 #             return (img4d, ms, md)
+
+#     def __repr__(self) -> str:
+#         return f"{self.__class__.__name__}()"
+
+
+# class Resize:
+#     def __init__(self, size: tuple[int, int, int]):
+#         self.size = size
+
+#     def __call__(self, vol: torch.Tensor, ms: torch.Tensor, md: torch.Tensor):
+#         NZ, NY, NX, NT = vol.shape
+#         nsize = (1, 1, NZ, NY, NX)
+#         ms_i = F.interpolate(ms.reshape(nsize), size=self.size, align_corners=True, mode='trilinear').squeeze()
+#         md_i = F.interpolate(md.reshape(nsize), size=self.size, align_corners=True, mode='trilinear').squeeze()
+#         vol_i = torch.zeros(size=(*self.size, NT), dtype=vol.dtype, device=vol.device)
+#         for t in range(NT):
+#             vol_i[:, :, :, t] = F.interpolate(vol[:, :, :, t].reshape(nsize), size=self.size, align_corners=True, mode='trilinear').squeeze()
+#         return (vol_i, ms_i, md_i)
+
+#     def __repr__(self) -> str:
+#         return f"{self.__class__.__name__}()"
+
+# class ResizeFlow3d:
+#     def __init__(self, size: tuple[int, int, int]):
+#         self.size = size
+
+#     def __call__(self, uvw: torch.Tensor):
+#         NZ, NY, NX, C = uvw.shape
+#         nsize = (1, 1, NZ, NY, NX)
+#         uvw_i = torch.zeros(size=(*self.size, C), dtype=uvw.dtype, device=uvw.device)
+#         for c in range(C):
+#             uvw_i[:, :, :, c] = F.interpolate(uvw[:, :, :, c].reshape(nsize), size=self.size, align_corners=True, mode='trilinear').squeeze()
+#         return uvw_i
 
 #     def __repr__(self) -> str:
 #         return f"{self.__class__.__name__}()"