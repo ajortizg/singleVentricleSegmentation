2dfac36b811e8ffb99560de79375646a4f402bac
Antonio Ortiz <toniortizg@gmail.com>
2024-06-28 13:27:25 +0200

    PropagationLoss module

--- svs/models/lit_unet.py
+++ svs/models/lit_unet.py
@@ -4,6 +4,7 @@ import torch
 
 from svs.models.unet import UNet
 from svs.modules.flow.warping import Warp
+from svs.modules.loss import PropagationLoss
 from svs.utils.constants import *
 
 
@@ -11,6 +12,8 @@ class LitUNet(pl.LightningModule):
     def __init__(
         self,
         epochs,
+        lambda_u: float,
+        gamma_p: float,
         lr: float = 1e-4,
         betas: Tuple[float, float] = (0.9, 0.999),
         weight_decay: float = 1e-4,
@@ -19,10 +22,10 @@ class LitUNet(pl.LightningModule):
         ** kwargs: torch.Any
     ):
         """
-        Initializes the LitUNet model.
-
         Args:
             epochs (int): Number of training epochs.
+            lambda_u (float): Weight for the unsupervised loss term.
+            gamma_p (float): Weight for the penalty term.
             lr (float): Learning rate for the optimizer.
             betas (Tuple[float, float]): Betas for the Adam optimizer.
             weight_decay (float): Weight decay for the optimizer.
@@ -35,6 +38,7 @@ class LitUNet(pl.LightningModule):
 
         self.model = UNet(**model_kwargs)
         self.warp = Warp(**warp_kwargs)
+        self.loss_fn = PropagationLoss(lambda_u, gamma_p)
 
         self.epochs = epochs
         self.lr = lr
@@ -48,7 +52,7 @@ class LitUNet(pl.LightningModule):
         return {"optimizer": optimizer, "lr_scheduler": scheduler}
 
     def forward(self, batch: Dict[str, Any]) -> Iterable[torch.Tensor]:
-        img, mi, mf, fflow, bflow, fts, bts = self.extract_batch_data(batch)
+        img, mi, mf, fflow, bflow, ftimes, btimes = self.extract_batch_data(batch)
 
         bs, ts, nz, ny, nx, _ = fflow.shape
         dtype = mi.dtype
@@ -56,6 +60,8 @@ class LitUNet(pl.LightningModule):
         batch_indices = torch.arange(bs)
 
         # Initialize tensors for forward and backward propagation
+        # mts -> forward
+        # mtts -> backward
         mts = torch.empty(size=(bs, ts + 1, 1, nz, ny, nx), dtype=dtype, device=device)
         mts[:, 0] = mi
         mtts = torch.empty_like(mts)
@@ -66,8 +72,8 @@ class LitUNet(pl.LightningModule):
         mhhs = torch.empty_like(mhs)
 
         for i in range(ts):
-            mts[:, i + 1], mhs[:, i] = self.propagation_step(mts[:, i], img, fflow[:, i], fts, i, batch_indices)
-            mtts[:, ts - i - 1], mhhs[:, ts - i - 1] = self.propagation_step(mtts[:, ts - i], img, bflow[:, i], bts, i, batch_indices)
+            mts[:, i + 1], mhs[:, i] = self.propagation_step(mts[:, i], img, fflow[:, i], ftimes, i, batch_indices)
+            mtts[:, ts - i - 1], mhhs[:, ts - i - 1] = self.propagation_step(mtts[:, ts - i], img, bflow[:, i], btimes, i, batch_indices)
 
         return mts, mtts, mhs, mhhs
 
@@ -86,16 +92,22 @@ class LitUNet(pl.LightningModule):
         mask: torch.Tensor,
         img: torch.Tensor,
         flow: torch.Tensor,
-        ts: torch.Tensor,
+        times: torch.Tensor,
         i: int,
         batch_indices: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """Performs a single propagation step (forward or backward)."""
         warped_mask = self.warp(mask, flow)
         # TODO: batch_indices is needed in fts?
-        x = torch.cat((img[batch_indices, ts[batch_indices, i + 1]], warped_mask), dim=1)
+        x = torch.cat((img[batch_indices, times[batch_indices, i + 1]], warped_mask), dim=1)
         next_mask, mh = self.model(x)
         return next_mask, mh
+
+    def forward_and_loss(self, batch, batch_idx) -> Iterable[torch.Tensor]:
+        y = self(batch)
+
+        offsets = torch.tensor(batch[OFFSET_KEY]).to(torch.long)
+        return self.loss_fn(y[0], y[1], y[2], y[3], offsets)
 
     def training_step(self, batch, batch_idx):
         mts, mtts, mhs, mhhs = self(batch)
---
+++ svs/modules/loss.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+from typing import Iterable
+
+
+class PropagationLoss(nn.Module):
+    def __init__(self, lambda_u: float, gamma_p: float):
+        """
+        Args:
+            lambda_unsup (float): Weight for the unsupervised loss term.
+            gamma_penalty (float): Weight for the penalty term.
+        """
+        super().__init__()
+
+        self.lambda_u = lambda_u
+        self.gamma_p = gamma_p
+        self.mse_fn = nn.MSELoss(reduction="sum")
+
+    def forward(
+        self,
+        mts: torch.Tensor,
+        mtts: torch.Tensor,
+        mhs: torch.Tensor,
+        mhhs: torch.Tensor,
+        offsets: torch.Tensor
+    ) -> Iterable[torch.Tensor]:
+        bs = mts.shape[0]
+        batch_indices = torch.arange(bs)
+
+        # Supervised term for known labels
+        mi = mts[batch_indices, 0]
+        mitt = mtts[batch_indices, offsets[batch_indices]]
+        sup_term_1 = self.mse_fn(mitt, mi)
+
+        mf = mtts[batch_indices, -1]
+        mft = mts[batch_indices, -offsets[batch_indices] - 1]
+        sup_term_2 = self.mse_fn(mft, mf)
+
+        # Unsupervised term for interior labels (no ground truth)
+        kl = mts.shape[1] - offsets
+        times_tildes = mts.shape[1]
+        times_hats = mhs.shape[1]
+
+        unsup_term = torch.as_tensor(0.0, device=mts.device)
+        pen_term = torch.as_tensor(0.0, device=mts.device)  # Penalization term
+
+        for b in range(bs):
+            # Self-supervised term
+            mt = mts[b, 1:times_tildes - offsets[b]]
+            mtt = mtts[b, 1 + offsets[b]:-1]
+            unsup_term += self.mse_fn(mt, mtt) / kl[b]
+
+            # Penalization term
+            mh = mhs[b, 0:times_hats - offsets[b]]
+            mhh = mhhs[b, 0:times_hats - offsets[b]]
+            pen_term += (mh.pow(2).sum() + mhh.pow(2).sum()) / kl[b]
+
+        sup_term = (sup_term_1 + sup_term_2) / bs
+        unsup_term *= (self.lambda_u / bs)
+        pen_term *= (self.gamma_p / bs)
+        loss = sup_term + unsup_term + pen_term
+
+        return loss, sup_term, unsup_term, pen_term
