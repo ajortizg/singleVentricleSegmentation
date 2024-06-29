5d67ee193f496f1252d07a495c81a35635124826
Antonio Ortiz <toniortizg@gmail.com>
2024-06-29 11:34:51 +0200

    early stopping on min metric loss_val/t

--- scripts/train.py
+++ scripts/train.py
@@ -3,6 +3,7 @@ import os.path as osp
 from omegaconf import DictConfig
 import lightning as pl
 from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
 
 
 @hydra.main(config_path="../conf", config_name="train", version_base=None)
@@ -14,7 +15,7 @@ def main(cfg: DictConfig):
     data = hydra.utils.instantiate(cfg["data"])
 
     logger = TensorBoardLogger(
-        osp.join(hydra.core.hydra_config.HydraConfig.get().runtime.output_dir, "tb_logs"),
+        save_dir=osp.join(hydra.core.hydra_config.HydraConfig.get().runtime.output_dir, "tb_logs"),
         name=model.__class__.__name__
     )
 
@@ -22,15 +23,26 @@ def main(cfg: DictConfig):
         accelerator="gpu",
         max_epochs=1000,
         min_epochs=1000,
+        callbacks=[
+            ModelCheckpoint(monitor="loss_val/t", save_top_k=1, verbose=True),
+            EarlyStopping(monitor="loss_val/t", mode="min", patience=cfg["patience"], verbose=True)
+        ],
         num_sanity_val_steps=-1,
         devices=cfg["gpu"],
         precision=32,
         logger=logger,
         log_every_n_steps=1,
+        check_val_every_n_epoch=1,
         enable_model_summary=True
     )
 
     trainer.fit(model, data)
+
+    trainer.checkpoint_callback
+
+    # best_ckpt = Path(checkpoint_cb.best_model_path)
+    # print(f'Best model checkpoint: {best_ckpt.name}')
+    # model = type(model).load_from_checkpoint(best_ckpt)
 
 
 if __name__ == "__main__":
